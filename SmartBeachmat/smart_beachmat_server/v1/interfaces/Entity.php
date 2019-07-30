41b08e6fa3caf49766ae1cb0559147164eba9fe6
Adam Dorogi-Kaposi <s4408496@student.uq.edu.au>
2019-07-30 15:54:12 +1000

    Add an Entity interface
    Add an Entity interface, which will be the skeleton of all entities
    stored and managed in the database.

---
+++ SmartBeachmat/smart_beachmat_server/v1/interfaces/Entity.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * An interface to define common functions for a database entity.
+ */
+interface Entity {
+    /**
+     * Adds an entity to the database.
+     * 
+     * @param attributes The array of attributes of the entity.
+     */
+    function create($attributes);
+
+    /**
+     * Gets an entity from the database.
+     * 
+     * @param id The ID (primary key) of the entity to retrieve.
+     * @return attributes The attributes of the entity.
+     */
+    function read($id);
+
+    /**
+     * Changes an entity in the database.
+     * 
+     * @param id The ID (primary key) of the entity to update.
+     * @param attributes The attributes of the entity to update.
+     */
+    function update($id, $attributes);
+
+    /**
+     * Remove an entity from the database.
+     * 
+     * @param id The ID (primary key) of the entity to delete.
+     */
+    function delete($id);
+}
+?>
