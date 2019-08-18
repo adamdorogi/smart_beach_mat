4112af10c90b8b50aad1922e3f1b63d019b47381
Adam Dorogi-Kaposi <s4408496@student.uq.edu.au>
2019-08-18 11:59:43 +1000

    Split lines to less than 80 characters
    Split comment, exceeding 80 characters, onto multiple lines

--- SmartBeachmat/smart_beachmat_app/lib/sign_up_scaffold.dart
+++ SmartBeachmat/smart_beachmat_app/lib/sign_up_scaffold.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:smart_beachmat_app/sign_up_form_body.dart';
 
 class SignUpScaffold extends StatelessWidget {
-  // Create a global key that uniquely identifies the Form widget and allows validation of the form.
-  final _formKey = GlobalKey<FormState>();
+  // Create a global key that uniquely identifies the Form widget and allows
+  // validation of the form.
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
