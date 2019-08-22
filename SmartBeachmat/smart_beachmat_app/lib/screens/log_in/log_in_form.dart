404d1254c9bb0c5681a61aa93ff0f389a04dd1e4
Adam Dorogi-Kaposi <s4408496@student.uq.edu.au>
2019-08-22 16:30:13 +1000

    Add log in screen
    Add log in screen to the application.

---
+++ SmartBeachmat/smart_beachmat_app/lib/screens/log_in/log_in_form.dart
@@ -0,0 +1,105 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart';
+
+import 'package:smart_beachmat_app/api_exception.dart';
+import 'package:smart_beachmat_app/api_service.dart';
+import 'package:smart_beachmat_app/main.dart';
+import 'package:smart_beachmat_app/widgets/sign_up_button.dart';
+
+class LogInForm extends StatefulWidget {
+  final GlobalKey<FormState> formKey;
+
+  const LogInForm(this.formKey);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _LogInFormState();
+  }
+}
+
+class _LogInFormState extends State<LogInForm> {
+  String _email;
+  String _password;
+
+  TextEditingController _emailController = new TextEditingController();
+  TextEditingController _passwordController = new TextEditingController();
+
+  bool _isButtonEnabled = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController.addListener(_validateForm);
+    _passwordController.addListener(_validateForm);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TextFormField(
+          controller: _emailController,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(labelText: 'Email'),
+          onSaved: (String value) => _email = value,
+        ),
+        TextFormField(
+          controller: _passwordController,
+          obscureText: true,
+          decoration: InputDecoration(labelText: 'Password'),
+          onSaved: (String value) => _password = value,
+        ),
+        SignUpButton(
+          child: Text('Log In'),
+          onPressed: _isButtonEnabled ? _submit : null,
+        )
+      ],
+    );
+  }
+
+  bool _validateEmail(String email) {
+    final Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    return RegExp(pattern).hasMatch(email);
+  }
+
+  bool _validatePassword(String password) {
+    return password.length >= 8;
+  }
+
+  void _validateForm() {
+    bool isFormValid = _validateEmail(_emailController.text) &&
+        _validatePassword(_passwordController.text);
+
+    if (_isButtonEnabled != isFormValid) {
+      setState(() {
+        _isButtonEnabled = isFormValid;
+      });
+    }
+  }
+
+  Future<void> _submit() async {
+    widget.formKey.currentState.save();
+
+    try {
+      Response response =
+          await ApiService().createToken(email: _email, password: _password);
+
+      String token = json.decode(response.body)['token'];
+      FlutterSecureStorage storage = FlutterSecureStorage();
+      await storage.write(key: 'token', value: token);
+
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => MyApp()));
+    } on ApiException catch (err) {
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text(err.message),
+        backgroundColor: Colors.red,
+      ));
+    }
+  }
+}
--- SmartBeachmat/smart_beachmat_app/lib/screens/log_in/log_in_scaffold.dart
+++ SmartBeachmat/smart_beachmat_app/lib/screens/log_in/log_in_scaffold.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:smart_beachmat_app/screens/log_in/log_in_scaffold.dart';
 
+import 'package:smart_beachmat_app/screens/log_in/log_in_form.dart';
+import 'package:smart_beachmat_app/screens/sign_up/email/sign_up_email_scaffold.dart';
 import 'package:smart_beachmat_app/widgets/left_app_bar.dart';
-import 'package:smart_beachmat_app/screens/sign_up/email/sign_up_email_form.dart';
 
-class SignUpEmailScaffold extends StatelessWidget {
+class LogInScaffold extends StatelessWidget {
   static GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
@@ -12,32 +12,36 @@ class SignUpEmailScaffold extends StatelessWidget {
     return Scaffold(
       appBar: LeftAppBar(
         context,
-        title: Text('Sign Up'),
+        title: Text('Log In'),
       ),
       body: Column(
         children: <Widget>[
           Form(
             key: _formKey,
-            child: SignUpEmailForm(_formKey),
+            child: LogInForm(_formKey),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text(
-                'Already have an account? ',
+                'Don\'t have an account? ',
                 style: Theme.of(context).primaryTextTheme.caption,
               ),
               InkWell(
-                onTap: () => Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: (context) => LogInScaffold())),
+                onTap: () => Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SignUpEmailScaffold(),
+                  ),
+                ),
                 child: Text(
-                  'Log in.',
+                  'Sign up.',
                   style: Theme.of(context)
                       .primaryTextTheme
                       .caption
                       .copyWith(color: Theme.of(context).primaryColor),
                 ),
-              )
+              ),
             ],
           ),
         ],
