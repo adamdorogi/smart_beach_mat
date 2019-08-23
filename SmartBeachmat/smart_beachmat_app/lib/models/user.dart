cf71f6eb34d6afacbfe38f41411044453e548ff6
Adam Dorogi-Kaposi <s4408496@student.uq.edu.au>
2019-08-23 18:11:53 +1000

    Re-format and add `fromJson`
    Re-format model and add `fromJson` constructor, which creates a user
    model from a given JSON map.

--- SmartBeachmat/smart_beachmat_app/lib/models/user.dart
+++ SmartBeachmat/smart_beachmat_app/lib/models/user.dart
@@ -1,13 +1,32 @@
 class User {
+  String id;
   String name;
   int skinType;
   String gender;
   String dob;
+  bool isOwner;
+  String createdOn;
 
-  User({this.name, this.skinType, this.gender, this.dob});
+  User(
+      {this.id,
+      this.name,
+      this.skinType,
+      this.gender,
+      this.dob,
+      this.isOwner,
+      this.createdOn});
+
+  User.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        skinType = int.parse(json['skin_type']),
+        gender = json['gender'],
+        dob = json['dob'],
+        isOwner = json['is_owner'],
+        createdOn = json['created_on'];
 
   @override
   String toString() {
-    return 'User(name: $name, skinType: $skinType, gender: $gender, dob: $dob)';
+    return 'User {id: $id, name: $name, skinType: $skinType, gender: $gender, dob: $dob, isOwner: $isOwner, createdOn: $createdOn}';
   }
 }
